796a55a31b81ca154c97054a922fe97aab73b892
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 06:34:41 +0500

    CouponStore added with default Coupons list.

---
+++ MinimalAPI.Demo/Data/CouponStore.cs
@@ -0,0 +1,13 @@
+﻿using MinimalAPI.Demo.Models;
+
+namespace MinimalAPI.Demo.Data
+{
+	public static class CouponStore
+	{
+		public static List<Coupon> Coupons = new List<Coupon>()
+		{
+			new Coupon { Id= 1, Name = "10 OFF", Percent = 10, IsActive = true },
+			new Coupon { Id= 2, Name = "20 OFF", Percent = 20, IsActive = true },
+		};
+	}
+}
