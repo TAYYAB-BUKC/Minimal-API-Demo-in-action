c2628c92b28adc98e0abc749cba94696c52b19f1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 08:44:39 +0500

    APIResponse model added.

---
+++ MinimalAPI.Demo/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MinimalAPI.Demo.Models
+{
+	public class APIResponse
+	{
+		public bool IsSuccess { get; set; }
+		public object Data { get; set; }
+		public HttpStatusCode StatusCode { get; set; }
+		public List<string> ErrorMessages { get; set; } = new();
+	}
+}
