a2e6f131f39a7b64e49b314c3b3803469b7ec54a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 09:26:09 +0500

    CouponUpdateDTO mappings added in Mapping Configuration.

--- MinimalAPI.Demo/Mappings/MappingConfiguration.cs
+++ MinimalAPI.Demo/Mappings/MappingConfiguration.cs
@@ -10,6 +10,7 @@ namespace MinimalAPI.Demo.Mappings
 		{
 			CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
 			CreateMap<Coupon, CouponDTO>().ReverseMap();
+			CreateMap<Coupon, CouponUpdateDTO>().ReverseMap();
 		}
 	}
 }
