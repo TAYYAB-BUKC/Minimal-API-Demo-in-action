5e9f48fee0e1963a3f8552dbd91823a12a088482
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 07:44:25 +0500

    Mapping configuration implemented for AutoMapper.

---
+++ MinimalAPI.Demo/Mappings/MappingConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MinimalAPI.Demo.DTOs;
+using MinimalAPI.Demo.Models;
+
+namespace MinimalAPI.Demo.Mappings
+{
+	public class MappingConfiguration : Profile
+	{
+		public MappingConfiguration()
+		{
+			CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
+			CreateMap<Coupon, CouponDTO>().ReverseMap();
+		}
+	}
+}
