5cfcba872315e4ff9d933557793425ff3f93c003
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 08:26:23 +0500

    CouponCreateValidator implemented.

---
+++ MinimalAPI.Demo/FluentValidations/CouponCreateValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MinimalAPI.Demo.DTOs;
+
+namespace MinimalAPI.Demo.FluentValidations
+{
+	public class CouponCreateValidator : AbstractValidator<CouponCreateDTO>
+	{
+		public CouponCreateValidator()
+		{
+			RuleFor(c => c.Name).NotEmpty();
+			RuleFor(c => c.Percent).InclusiveBetween(1, 100);
+		}
+	}
+}
