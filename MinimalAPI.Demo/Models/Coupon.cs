794b6616892dc669f15d63ab736d2f04f931f5c8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 06:31:22 +0500

    Coupon model added.

---
+++ MinimalAPI.Demo/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalAPI.Demo.Models
+{
+	public class Coupon
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Percent { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public DateTime? LastUpdatedDate { get; set; }
+	}
+}
