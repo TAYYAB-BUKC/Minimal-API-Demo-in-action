7eca9b3cb3eaac2d9101e65d252f2dcefa497e2f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-24 07:31:05 +0500

    DTOs added for Coupon.

---
+++ MinimalAPI.Demo/DTOs/CouponDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalAPI.Demo.DTOs
+{
+	public class CouponDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Percent { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime? CreatedDate { get; set; }
+	}
+}
