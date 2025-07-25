948ac11d91a0d35748615dd8fca27c71afec360c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-26 00:40:25 +0500

    IAuthRepository added.

---
+++ MinimalAPI.Demo/Repository/IRepository/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using MinimalAPI.Demo.DTOs;
+
+namespace MinimalAPI.Demo.Repository.IRepository
+{
+	public interface IAuthRepository
+	{
+		Task<bool> IsUserUnique(string username);
+		Task<LoginResponseDTO> Authenticate(LoginRequestDTO request);
+		Task<UserDTO> Register(RegistrationRequestDTO request);
+	}
+}
