9cd56a7ecf07100d4d3e74cb5ec0e46288ad2e69
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-25 16:05:21 +0500

    ICouponRepository added.

---
+++ MinimalAPI.Demo/Repository/IRepository/ICouponRepository.cs
@@ -0,0 +1,15 @@
+﻿using MinimalAPI.Demo.Models;
+
+namespace MinimalAPI.Demo.Repository.IRepository
+{
+	public interface ICouponRepository
+	{
+		Task<IEnumerable<Coupon>> GetCouponsAsync();
+		Task<Coupon> GetCouponByIdAsync(int id);
+		Task<Coupon> GetCouponByNameAsync(string name);
+		Task AddCouponAsync(Coupon coupon);
+		Task UpdateCouponAsync(Coupon coupon);
+		Task DeleteCouponByIdAsync(Coupon coupon);
+		Task SaveChangesAsync();
+	}
+}
