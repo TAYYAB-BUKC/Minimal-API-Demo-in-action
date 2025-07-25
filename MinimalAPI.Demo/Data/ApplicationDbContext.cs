91dfaabb1f147e121fdf063ed8737a3722cd94d2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-25 15:38:19 +0500

    ApplicationDbContext initial implementation added.

---
+++ MinimalAPI.Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Demo.Models;
+
+namespace MinimalAPI.Demo.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<Coupon> Coupons { get; set; }
+	}
+}
