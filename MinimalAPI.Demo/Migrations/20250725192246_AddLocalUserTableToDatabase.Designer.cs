9e1da1995c70ed3dfab8f34b2e23ecace0d3d7ab
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-26 00:23:29 +0500

    Migration added for LocalUsers table.

--- MinimalAPI.Demo/Migrations/20250725192246_AddLocalUserTableToDatabase.Designer.cs
+++ MinimalAPI.Demo/Migrations/20250725192246_AddLocalUserTableToDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MinimalAPI.Demo.Data;
 
@@ -11,9 +12,11 @@ using MinimalAPI.Demo.Data;
 namespace MinimalAPI.Demo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250725192246_AddLocalUserTableToDatabase")]
+    partial class AddLocalUserTableToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
