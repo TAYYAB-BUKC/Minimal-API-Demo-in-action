87a83c65ffae9f85a49076ab6cc02bcac1ef4ec0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-25 15:43:54 +0500

    Migration added for Coupon table.

---
+++ MinimalAPI.Demo/Migrations/20250725104321_AddCouponTableToDatabase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MinimalAPI.Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCouponTableToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Percent = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "CreatedDate", "IsActive", "LastUpdatedDate", "Name", "Percent" },
+                values: new object[,]
+                {
+                    { 1, null, true, null, "10 OFF", 10 },
+                    { 2, null, true, null, "20 OFF", 20 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
